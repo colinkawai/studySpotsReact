7a34934f5fddd8b461484b47cd3a6db340609d20
colingtr <kkawai@ucsc.edu>
2020-04-10 20:35:52 -0700

    commit

--- backend/server.js
+++ backend/server.js
@@ -28,9 +28,11 @@ db.once("open", () => console.log("connected to the database"));
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("/mapreacdt/build/"));
+  app.use(
+    express.static(path.join(__dirname, "../mapreacdt/build/index.html"))
+  );
   app.get("*", (req, res) => {
-    res.sendFile("index.html", { root: "/mapreacdt/build/" });
+    res.sendFile(path.join(__dirname + "../mapreacdt/build/index.html"));
   });
 }
 // (optional) only made for logging and
